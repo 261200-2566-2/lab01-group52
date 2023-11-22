ff0b20cdf731831ae421a8a5a1b929cceca554ed
aomchanun <omsinjah@gmail.com>
2023-11-22 23:43:04 +0700

    implement sort-method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static int[] sorted(int[] arr, int n){ //bubble sort
+        boolean swap;
+        for(int i=0; i<n-1; i++){
+            swap = false;
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap) break;
         }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = -1;
+        while(n<0){
+            System.out.print("Enter the number of integers(n): ");
+            n = s.nextInt();
+            if(n<0) {
+                System.out.println("Please enter a non-negative integer");
+            }
+        }
+
+        int[] arr = new int[n];
+        for (int j=0; j<n; j++) {
+            System.out.print("Enter integer(s): ");
+            arr[j] = s.nextInt();
+        }
+
+        arr = sorted(arr, n);
+
+        for (int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+
+        //Reference: https://www.geeksforgeeks.org/bubble-sort/
     }
 }
